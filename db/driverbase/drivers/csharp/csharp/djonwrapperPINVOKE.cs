40b17ea905045495f64f539d45a3c8707cb61843
Juan Pablo Crossley <crossleyjuan@gmail.com>
2012-11-25 23:40:08 -0500

    csharp 0.120121125 generated

--- db/driverbase/drivers/csharp/csharp/djonwrapperPINVOKE.cs
+++ db/driverbase/drivers/csharp/csharp/djonwrapperPINVOKE.cs
@@ -546,6 +546,9 @@ class djonwrapperPINVOKE {
   [DllImport("djonwrapper", EntryPoint="CSharp_DjondbConnection_update__SWIG_1")]
   public static extern bool DjondbConnection_update__SWIG_1(HandleRef jarg1, string jarg2, string jarg3, HandleRef jarg4);
 
+  [DllImport("djonwrapper", EntryPoint="CSharp_DjondbConnection_remove")]
+  public static extern bool DjondbConnection_remove(HandleRef jarg1, string jarg2, string jarg3, string jarg4, string jarg5);
+
   [DllImport("djonwrapper", EntryPoint="CSharp_DjondbConnection_dropNamespace")]
   public static extern bool DjondbConnection_dropNamespace(HandleRef jarg1, string jarg2, string jarg3);
 
