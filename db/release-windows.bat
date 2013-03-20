bb02e8dc5f575b20bc374f5fdb99cd74418b2708
Juan Pablo Crossley <crossleyjuan@gmail.com>
2013-03-19 19:54:42 -0500

    Changes to allow windows x64 compilation

--- db/release-windows.bat
+++ db/release-windows.bat
@@ -5,11 +5,34 @@ cd %PATH_SRC_STARTUP%\driverbase\drivers
 call update.bat
 
 cd %PATH_SRC_STARTUP%
-%PATH_MSBUILD%\msbuild %PATH_SRC_STARTUP%\windows\djondb_vs2008.sln /t:Rebuild /p:Configuration=Release /p:Platform=Win32
 
+:Win32
+echo starting win32
+SET PLATFORM=Win32
+SET NEXT_STEP=x64
+GOTO ExecuteJob
+
+:x64
+echo starting x64
+SET PLATFORM=x64
+SET NEXT_STEP=END
+GOTO ExecuteJob
+
+GOTO END
+
+:ExecuteJob
+%PATH_MSBUILD%\msbuild %PATH_SRC_STARTUP%\windows\djondb_vs2008.sln /t:Rebuild /p:Configuration=Release /p:Platform=%PLATFORM%
+
+SET RELEASE_DIR=%PATH_SRC_STARTUP%\windows\Release
+
+xcopy %PATH_SRC_STARTUP%\windows\djonjavadriver.dll "%PATH_SRC_STARTUP%\driverbase\drivers\java\native\vs2008\"
 cd %PATH_SRC_STARTUP%\driverbase\drivers\java\java
 call "%ANT_PATH%\bin\ant"
 cd %PATH_SRC_STARTUP%
+
+mkdir "%RELEASE_DIR%"
+xcopy /y "%PATH_SRC_STARTUP%\Windows\libs\%PLATFORM%\*.*" "%RELEASE_DIR%\"
+xcopy /y "%PATH_SRC_STARTUP%\third_party\libs\%PLATFORM%\pthreadVC2.dll" "%RELEASE_DIR%\"
 
 "%INNO_PATH%\compil32" /cc "%PATH_SRC_STARTUP%\windows\installer\inno script.iss"
 "%INNO_PATH%\compil32" /cc "%PATH_SRC_STARTUP%\windows\installer\csharp driver.iss"
@@ -22,3 +45,14 @@ del /Q   "%OUTPUTDIR%\*.*"
 mkdir  "%OUTPUTDIR%"
 copy   "%PATH_SRC_STARTUP%\windows\installer\output\*" "%OUTPUTDIR%"
 copy   "%PATH_SRC_STARTUP%\driverbase\drivers\java\java\dist\lib\djondb_java.jar" "%OUTPUTDIR%\djondb_client_Windows_i686.jar"
+
+echo Changing names to target the platform
+move "%PATH_SRC_STARTUP%\windows\output\djondb_client_Windows_i686.jar" "%PATH_SRC_STARTUP%\windows\output\djondb_client_Windows_%PLATFORM%.jar" 
+move "%PATH_SRC_STARTUP%\windows\output\setup_djoncsharp.exe" "%PATH_SRC_STARTUP%\windows\output\setup_djoncsharp_%PLATFORM%.exe" 
+move "%PATH_SRC_STARTUP%\windows\output\setup_djondb.exe" "%PATH_SRC_STARTUP%\windows\output\setup_djondb_%PLATFORM%.exe" 
+
+GOTO %NEXT_STEP%
+
+:END
+
+Echo Done!
