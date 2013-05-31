c6a346a294ff773dea4c786bc8d98baf92ab3abe
Cross <crossleyjuan@gmail.com>
2013-05-31 08:28:40 -0500

    Changes to windows compilation scripts, now it's using cmake, and the third_party directory.

---
+++ db/README.txt
@@ -0,0 +1,44 @@
+djondb
+======
+
+djondb is a NoSQL database implementation which follows the document datastore
+model, the main goal is to achieve all the promises of NoSQL stores: performance,
+scalability, etc, but focusing on consistency and multi document transactional.
+
+If you need further information please refer to the site http://djondb.com there
+you will find installation/compilation instructions, full documentation, etc.
+
+
+Contributing
+------------
+
+Want to contribute? Great! start by forking the repo on GitHub:
+
+http://github.com/djondb/djondb
+
+* Clone your fork
+* Do the hack/change you want
+* Add some tests and be sure everything passes your tests
+* If you add new functionality, document it in the README
+* If necessary, rebase your commits into chunks, without errors
+* Push the branch up to GitHub
+* Send a pull request to djondb/djondb
+
+There're other ways to contribute, if you need a new feature or something is missing just send your feedback
+using the support group https://groups.google.com/forum/#!forum/djondb-support or using the feedback option
+at the right hand of the djondb.com site.
+
+
+Compiling
+---------
+
+To do the compilation you will need the following dependencies:
+
+antlr3c Ant C runtime 3.2
+google v8 javascript engine
+boost libraries
+
+to compile the google v8 use:
+
+make --component=shared native
+
--- db/build.bat
+++ db/build.bat
@@ -1,3 +1,109 @@
+@echo off
+
+if [%1] ==[] goto usage
+
+set x32=false
+if [%1] == [-x32] (
+   set x32=true
+)
+if [%2] == [-x32] (
+   set x32=true
+)
+set x64=false
+if [%1] == [-x64] (
+   set x64=true
+)
+if [%2] == [-x64] (
+   set x64=true
+)
+
+call setenv.bat
+
+cd %PATH_SRC_STARTUP%\driverbase\drivers
+call update.bat
+
+cd %PATH_SRC_STARTUP%
+
+:Win32
+if %x32% == true (
+	echo starting win32
+	SET PLATFORM=Win32
+	SET NEXT_STEP=x64
+	GOTO ExecuteJob
+)
+
+:x64
+if %x64% == true (
+	echo starting x64
+	SET PLATFORM=x64
+	SET NEXT_STEP=END
+	GOTO ExecuteJob
+)
+
+GOTO END
+
+:ExecuteJob
+mkdir build%PLATFORM%
+cd build%PLATFORM%
+cmake .. -DCMAKE_BUILD_TYPE=%CONFIGURATION%
+pause
+
+%PATH_MSBUILD%\msbuild %PATH_SRC_STARTUP%\build\%PLATFORM%\djondb.sln /t:Rebuild /p:Configuration=Release /p:Platform=%PLATFORM%
+
+SET RELEASE_DIR=%PATH_SRC_STARTUP%\windows\Release
+
+rem JAVA
+rem xcopy %PATH_SRC_STARTUP%\windows\djonjavadriver.dll "%PATH_SRC_STARTUP%\driverbase\drivers\java\native\vs2008\"
+rem cd %PATH_SRC_STARTUP%\driverbase\drivers\java\java
+rem call "%ANT_PATH%\bin\ant"
+rem
+cd %PATH_SRC_STARTUP%
+
+cd build%PLATFORM%
+cpack
+
+rem mkdir "%RELEASE_DIR%"
+rem xcopy /y "%PATH_SRC_STARTUP%\Windows\libs\%PLATFORM%\*.*" "%RELEASE_DIR%\"
+rem if x32 == true (
+    rem the Csharp driver is been copied on other folder
+    rem xcopy /y "%PATH_SRC_STARTUP%\Windows\libs\x86\*.*" "%RELEASE_DIR%\"
+)
+rem xcopy /y "%PATH_SRC_STARTUP%\third_party\libs\%PLATFORM%\pthreadVC2.dll" "%RELEASE_DIR%\"
+
+"%INNO_PATH%\compil32" /cc "%PATH_SRC_STARTUP%\windows\installer\inno script.iss"
+"%INNO_PATH%\compil32" /cc "%PATH_SRC_STARTUP%\windows\installer\csharp driver.iss"
+
+REM Everything done, now it's time to create the output
+REM
+
+echo Creating output dir
+del /Q   "%OUTPUTDIR%\*.*"
+mkdir  "%OUTPUTDIR%"
+copy   "%PATH_SRC_STARTUP%\windows\installer\output\*" "%OUTPUTDIR%"
+copy   "%PATH_SRC_STARTUP%\driverbase\drivers\java\java\dist\lib\djondb_java.jar" "%OUTPUTDIR%\djondb_client_Windows_i686.jar"
+
+echo Changing names to target the platform
+move "%PATH_SRC_STARTUP%\windows\output\djondb_client_Windows_i686.jar" "%PATH_SRC_STARTUP%\windows\output\djondb_client_Windows_%PLATFORM%.jar" 
+move "%PATH_SRC_STARTUP%\windows\output\setup_djoncsharp.exe" "%PATH_SRC_STARTUP%\windows\output\setup_djoncsharp_%PLATFORM%.exe" 
+move "%PATH_SRC_STARTUP%\windows\output\setup_djondb.exe" "%PATH_SRC_STARTUP%\windows\output\setup_djondb_%PLATFORM%.exe" 
+
+GOTO %NEXT_STEP%
+
+:usage
+
+Echo Usage: release-windows [-x32] [-x64]
+goto exit
+
+:END
+
+Echo Done!
+
+:Exit
+
+
+
+
+
 set CONFIGURATION=Release
 rem set CONFIGURATION=Debug
 
