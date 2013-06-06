d0484b448cf3e15800191b267fc250a91f0702e6
Cross <cross@djondb.com>
2013-06-05 22:44:44 -0500

    test performance changed to use names and last names files

--- db/driverbase/tests/testperformance.cpp
+++ db/driverbase/tests/testperformance.cpp
@@ -18,6 +18,7 @@
 
 #include <iostream>
 #include "djondb_client.h"
+#include "fileinputstream.h"
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -31,6 +32,39 @@ class TestPerfomance {
 		TestPerfomance() {
 		}
 
+		std::vector<std::string>* generateNames(int number) {
+			// this will avoid overriding previously generated names and keep consistent the results
+				FileInputStream* fisNames = new FileInputStream("names.csv", "r");
+				const char* fullNames = fisNames->readFull();
+				FileInputStream* fisLastNames = new FileInputStream("last.csv", "r");
+				const char* fullLast = fisLastNames->readFull();
+
+				std::vector<std::string> names = split(fullNames, "\r");
+				cout << names.size() << endl;
+				std::vector<std::string> lastNames = split(fullLast, "\r");
+				cout << lastNames.size() << endl;
+
+				std::vector<std::string>* generated = new std::vector<std::string>();
+				for (int x = 0; x < number; x++) {
+					int i = rand() % names.size();
+					std::string name = names.at(i);
+
+					i = rand() % lastNames.size();
+					std::string lastName = lastNames.at(i);
+
+					std::string fullName = name + " " + lastName;
+
+					generated->push_back(fullName);
+				}
+
+				fisNames->close();
+				fisLastNames->close();
+
+				delete fisNames;
+				delete fisLastNames;
+				return generated;
+		}
+
 		void testPerfomance(int port, int top = 10000000) {
 			DjondbConnection* conn = DjondbConnectionManager::getConnection("localhost", port);
 
@@ -42,40 +76,42 @@ class TestPerfomance {
 			// 1k inserts
 			//
 			Logger* log = getLogger(NULL);
-			int tests[] = { 10, 100, 1000, 10000, 100000, 1000000, 10000000};
-			for (int i = 0; i < 7; i++) {
-				if (tests[i] > top) {
-					break;
-				}
-				log->startTimeRecord();
-				cout << "Testing performance over: " << tests[i] << " inserts" << endl;
-				for (int x = 0; x < tests[i]; x++) {
-					BSONObj obj;
-					char* text = (char*)malloc(1001);
-					memset(text, 0, 1001);
-					memset(text, 'a', 1000);
+			std::vector<std::string>* names = generateNames(top);
 
-					obj.add("t", x);
+			log->startTimeRecord();
+			cout << "Testing performance over: " << top << " inserts" << endl;
+			for (int x = 0; x < top; x++) {
+				BSONObj obj;
+				char* text = (char*)malloc(1001);
+				memset(text, 0, 1001);
+				memset(text, 'a', 1000);
 
-					obj.add("text", text);
+				obj.add("t", x);
 
-					conn->insert("db", "test.performance", obj);
-					free(text);
-				}
+				obj.add("text", text);
+				obj.add("name", const_cast<char*>(names->at(x).c_str()));
 
-				log->stopTimeRecord();
+				conn->insert("db", "test.performance", obj);
+				free(text);
+			}
 
-				DTime time = log->recordedTime();
+			log->stopTimeRecord();
 
-				cout << "Total secs: " << time.totalSecs() << endl;
-				if (time.totalSecs() > 0) {
-					cout << "Performance over: " << tests[i] << " inserts:" << (tests[i] / time.totalSecs()) << endl;
-				} else {
-					cout << "Total time is not enough to do some calculations" << endl;
-				}
-				if ((time.totalSecs() > 0) && ((tests[i] / time.totalSecs()) < 16000))  {
-					cout << "Performance is not good enough" << endl;
-				}
+			cout << "doing count" << endl;
+			BSONArrayObj* temp = conn->find("db", "test.performance", "count", "");
+
+			cout << "Inserted: " << top << ", returned: " << temp->get(0)->getInt("count") << endl;
+
+			DTime time = log->recordedTime();
+
+			cout << "Total secs: " << time.totalSecs() << endl;
+			if (time.totalSecs() > 0) {
+				cout << "Performance over: " << top << " inserts:" << (top / time.totalSecs()) << endl;
+			} else {
+				cout << "Total time is not enough to do some calculations" << endl;
+			}
+			if ((time.totalSecs() > 0) && ((top / time.totalSecs()) < 16000))  {
+				cout << "Performance is not good enough" << endl;
 			}
 
 			//			conn->shutdown();
