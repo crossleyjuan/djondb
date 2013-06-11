d7f9ec2ddd49426e288ccbd62803e75837f41023
Cross <cross@djondb.com>
2013-06-10 21:17:14 -0400

    buildv8 updated to compile on x86 or x64

--- db/third_party/buildv8.sh
+++ db/third_party/buildv8.sh
@@ -3,9 +3,20 @@
 tar xvfz v8.tar.gz
 cd v8
 
-python build/gyp_v8 -Dtarget_arch=x64 -Dcomponent=static_library
-make x64.release
-cp -f out/x64.release/libv8_base*.a ../libs/libv8_base.a
-cp -f out/x64.release/libv8_nosnapshot*.a ../libs/libv8_nosnapshot.a
-cp -f out/x64.release/libv8_snapshot*.a ../libs/libv8_snapshot.a
+MACHINE=`uname -i`
+X64='x86_64'
+
+echo ${MACHINE}
+if [ "${MACHINE}" = "${X64}" ];
+then
+	python build/gyp_v8 -Dtarget_arch=x64 -Dcomponent=static_library
+	make x64.release
+else
+	python build/gyp_v8 -Dtarget_arch=ia32 -Dcomponent=static_library
+	make ia32.release
+fi
+cp -f `find out -iwholename '*/libv8_base*.a'` ../libs/libv8_base.a
+cp -f `find out -iwholename '*/libv8_nosnapshot*.a'`  ../libs/libv8_nosnapshot.a
+cp -f `find out -iwholename '*/libv8_snapshot*.a'` ../libs/libv8_snapshot.a
 cp -f include/*.h ../includes/
+
